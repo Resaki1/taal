6686464a7d7a78ac17fbcfe2d58b57830383bd14
Resaki1 <christian.reski@t-online.de>
2022-06-12 12:04:52 +0200

    reusing geometries and materials now

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,3 +1,12 @@
+import {
+  tile,
+  water,
+  sand,
+  meadow,
+  forest,
+  mountain,
+} from "../../materials/materials";
+
 export type TileProps = {
   x: number;
   y: number;
@@ -5,24 +14,20 @@ export type TileProps = {
 };
 
 export const Tile = ({ x, y, type }: TileProps) => {
+  const material =
+    type <= -0.2
+      ? water
+      : type < -0.15
+      ? sand
+      : type < 0.1
+      ? meadow
+      : type < 0.6
+      ? forest
+      : mountain;
+
   return (
     <group position={[x, 0, y]}>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial
-          color={
-            type <= -0.2
-              ? "blue"
-              : type < -0.15
-              ? "yellow"
-              : type < 0.1
-              ? "lightgreen"
-              : type < 0.6
-              ? "green"
-              : "grey"
-          }
-        />
-      </mesh>
+      <mesh geometry={tile} material={material}></mesh>
     </group>
   );
 };
---
+++ src/materials/materials.tsx
@@ -0,0 +1,9 @@
+import * as THREE from "three";
+
+export const tile = new THREE.BoxBufferGeometry(1, 1, 1);
+
+export const water = new THREE.MeshStandardMaterial({ color: "blue" });
+export const sand = new THREE.MeshStandardMaterial({ color: "yellow" });
+export const meadow = new THREE.MeshStandardMaterial({ color: "lightgreen" });
+export const forest = new THREE.MeshStandardMaterial({ color: "green" });
+export const mountain = new THREE.MeshStandardMaterial({ color: "grey" });
