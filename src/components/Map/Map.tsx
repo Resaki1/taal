2ac544d001b3b38358f583373581273c23feaf73
Resaki1 <christian.reski@t-online.de>
2022-06-14 23:03:09 +0200

    refactor

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,62 +1,53 @@
-import { Html } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useEffect, useState, useTransition } from "react";
-import { Ray, Vector3 } from "three";
-import { Tile, TileProps } from "../Tile/Tile";
+import { useState, useTransition } from "react";
+import { Vector3 } from "three";
+import { Tile } from "../Tile/Tile";
 
 export const Map = () => {
-  const map: {
-    tiles: TileProps[];
-  } = {
-    tiles: [],
-  };
-
-  const MAPSIZE = 96;
+  const RENDER_DISTANCE = 32;
 
   const noisejs = require("noisejs");
 
   const terrain = new noisejs.Noise(0);
   const { camera } = useThree();
   const [centerBlock, setCenterBlock] = useState(new Vector3(9999, 0, 0));
-  const [isPending, startTransition] = useTransition();
+  const [, startTransition] = useTransition();
 
-  const [test, setTest] = useState([[0, 0]]);
-
-  for (var x = -MAPSIZE / 2; x < MAPSIZE / 2; x++) {
-    for (var y = -MAPSIZE / 2; y < MAPSIZE / 2; y++) {
-      map.tiles.push({
-        x,
-        y,
-        type: terrain.perlin2(x / 24, y / 24),
-      });
-    }
-  }
-
-  const size = 48;
+  const [map, setMap] = useState([[0, 0]]);
 
   const target = new Vector3();
+  const position = new Vector3();
   useFrame(() => {
-    const position = camera.position.clone();
+    position.set(camera.position.x, camera.position.y, camera.position.z);
     const direction = camera.getWorldDirection(target);
     const ray = direction.multiplyScalar(camera.position.y / direction.y);
     const newCenter = position.sub(ray).round();
 
-    if (centerBlock.distanceTo(newCenter) > 8) {
+    if (centerBlock.distanceTo(newCenter) > 2) {
       setCenterBlock(newCenter);
 
       let newMap: number[][] = [];
-      for (var x = newCenter.x - size; x < newCenter.x + (size + 1); x++) {
-        for (var y = newCenter.z - size; y < newCenter.z + (size + 1); y++) {
-          newMap.push([x, y]);
+      for (
+        var x = newCenter.x - RENDER_DISTANCE;
+        x < newCenter.x + (RENDER_DISTANCE + 1);
+        x++
+      ) {
+        for (
+          var y = newCenter.z - RENDER_DISTANCE;
+          y < newCenter.z + (RENDER_DISTANCE + 1);
+          y++
+        ) {
+          if (target.set(x, 0, y).distanceTo(newCenter) < RENDER_DISTANCE)
+            newMap.push([x, y]);
         }
       }
-      startTransition(() => setTest(newMap));
+      startTransition(() => setMap(newMap));
     }
   });
 
   return (
     <>
-      {test.map((tile) => (
+      {map.map((tile) => (
         <Tile
           key={`${tile[0]}/${tile[1]}`}
           x={tile[0]}
