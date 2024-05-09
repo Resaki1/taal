66f0509ec4f179060db2b7e5adcfb2b62047a046
Resaki1 <christian.reski@t-online.de>
2024-05-09 18:38:34 +0200

    fixed table semantics of ressource menu

--- src/components/RessourceMenu/RessourceMenu.tsx
+++ src/components/RessourceMenu/RessourceMenu.tsx
@@ -7,61 +7,71 @@ export const RessourceMenu = () => {
   const buildingOutputs = useStore((state) => state.buildingOutputs);
   const reset = useStore((state) => state.reset);
 
-  const valueSmoothener: (buildingsOutputValue: number) => number = function (
-    buildingsOutputValue): number {
-    return (Math.round((buildingsOutputValue * 6000) / 100))
-  }
+  const valueSmoothener = (buildingsOutputValue: number) => Math.round((buildingsOutputValue * 6000) / 100);
 
-  const remainingRessources: (ressourcesPerMinute: number, totalRessources: number) => string = function (
-    ressourcesPerMinute: number,
-    totalRessources: number,
-
-  ): string {
+  const remainingRessources = (ressourcesPerMinute: number, totalRessources: number) => {
     if (ressourcesPerMinute < 0) {
-
-      return (Math.ceil(totalRessources / (ressourcesPerMinute * -1)) + "min")
-
+      return Math.ceil(totalRessources / (ressourcesPerMinute * -1)) + 'min';
     }
 
-    return ("♾");
+    return '♾';
   };
 
   return (
     <div className="ressource-menu">
       <table>
-        <tr>
-          <th></th>
-          <th>💵</th>
-          <th>🪵</th>
-          <th>🪨</th>
-          <th>🍟</th>
-          <th></th>
-          <th>🧑‍🦱</th>
-        </tr>
+        <thead>
+          <tr>
+            <th></th>
+            <th>💵</th>
+            <th>🪵</th>
+            <th>🪨</th>
+            <th>🍟</th>
+            <th></th>
+            <th>🧑‍🦱</th>
+          </tr>
+        </thead>
 
-        <tr>
-          <td><b>Total</b></td>
-          <td><b>{Math.floor(ressources.gold)}</b> ({valueSmoothener(buildingOutputs.gold)})</td>
-          <td><b>{Math.floor(ressources.wood)}</b> ({valueSmoothener(buildingOutputs.wood)})</td>
-          <td><b>{Math.floor(ressources.stone)}</b> ({valueSmoothener(buildingOutputs.stone)})</td>
-          <td><b>{Math.floor(ressources.food)}</b> ({valueSmoothener(buildingOutputs.food)})</td>
-          <td></td>
-          <td>{Math.floor(ressources.villager)}</td>
-        </tr>
-        <tr>
-          <td><b>Remaining</b></td>
-          <td>{remainingRessources(valueSmoothener(buildingOutputs.gold), Math.floor(ressources.gold))} </td>
-          <td>{remainingRessources(valueSmoothener(buildingOutputs.wood), Math.floor(ressources.wood))}</td>
-          <td>{remainingRessources(valueSmoothener(buildingOutputs.stone), Math.floor(ressources.stone))}</td>
-          <td>{remainingRessources(valueSmoothener(buildingOutputs.food), Math.floor(ressources.food))}</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <td colSpan="7">
-          <button onClick={() => reset()}>Restart</button>
-        </td>
+        <tbody>
+          <tr>
+            <td>
+              <b>Total</b>
+            </td>
+            <td>
+              <b>{Math.floor(ressources.gold)}</b> ({valueSmoothener(buildingOutputs.gold)})
+            </td>
+            <td>
+              <b>{Math.floor(ressources.wood)}</b> ({valueSmoothener(buildingOutputs.wood)})
+            </td>
+            <td>
+              <b>{Math.floor(ressources.stone)}</b> ({valueSmoothener(buildingOutputs.stone)})
+            </td>
+            <td>
+              <b>{Math.floor(ressources.food)}</b> ({valueSmoothener(buildingOutputs.food)})
+            </td>
+            <td></td>
+            <td>{Math.floor(ressources.villager)}</td>
+          </tr>
+          <tr>
+            <td>
+              <b>Remaining</b>
+            </td>
+            <td>{remainingRessources(valueSmoothener(buildingOutputs.gold), Math.floor(ressources.gold))} </td>
+            <td>{remainingRessources(valueSmoothener(buildingOutputs.wood), Math.floor(ressources.wood))}</td>
+            <td>{remainingRessources(valueSmoothener(buildingOutputs.stone), Math.floor(ressources.stone))}</td>
+            <td>{remainingRessources(valueSmoothener(buildingOutputs.food), Math.floor(ressources.food))}</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={7}>
+              <button onClick={() => reset()}>Restart</button>
+            </td>
+          </tr>
+        </tfoot>
       </table>
     </div>
   );
 };
-
